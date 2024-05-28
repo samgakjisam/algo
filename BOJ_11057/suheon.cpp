4d7e5487aa2850efb98906e7afef218d50934f4e
임수헌 <tngjs987@gmail.com>
2024-05-28 13:29:16 +0900

    ♻️refactor : BOJ10574오버플로우 해결

--- BOJ_11057/suheon.cpp
+++ BOJ_11057/suheon.cpp
@@ -7,35 +7,24 @@ using namespace std;
 int main() {
 
 	int num = 0;
-	long long ans = 0;
-	long long arr[1002][10] = { 0, };
-
-	for (int i = 0; i < 1001; i++) {
-		for (int j = 0; j < 10; j++) {
-			arr[i][j] = 1;
-		}
-	}
-
+	int ans = 0;
+	int arr[1001][10] = { 0, };
 	cin >> num;
 
-	for (int i = 2; i < num+2; i++) {
-		long long temp = 0;
-		ans = 0;
-		
-		// 다 더해서 다음 행의 첫 번째 열로 보내기
-		for (int j = 0; j < 10; j++) {
-			
-			temp += arr[i - 1][j];
-			ans += arr[i - 1][j];
-		}
+	for (int i = 0; i < 10; i++) {
+		arr[1][i] = i+1;
+	}
 
-		arr[i][0] = temp;
-		
-		// 이전 행에서 빼기
-		for (int j = 1; j < 10; j++) {
-			arr[i][j] = arr[i][j - 1] - arr[i - 1][j - 1];
+	for (int i = 2; i < num+1; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (j == 0) {
+				arr[i][j] = 1;
+				continue;
+			}
+			arr[i][j] = (arr[i][j - 1] + arr[i - 1][j]) % 10007;
 		}
 	}
 
-	cout << ans;
+
+	cout << arr[num][9];
 }
