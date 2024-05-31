8127c6eaf6f9049073b827eb643f53e6079b1fbf
calendar2 <zoro94317@gmail.com>
2024-05-31 15:25:49 +0900

    BOJ_1927: Java

---
+++ BOJ_1927/minjae.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0);
+                } else {
+                    sb.append(pq.poll());
+                }
+                sb.append("\n");
+            } else {
+                pq.add(x);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
