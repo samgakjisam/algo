e8d532427137ce8c092ae90158de9f8a05a5b85c
syhuni <74291750+syhuni@users.noreply.github.com>
2024-07-08 18:16:16 +0900

    BOJ_15652: cpp

---
+++ BOJ_15652/yeonghun.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+
+void dfs(int idx, int cnt) {
+	if (cnt == m) {
+		for (const auto& num : v) {
+			cout << num << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = idx; i <= n; i++) {
+		v.push_back(i);
+		dfs(i, cnt + 1);
+		v.pop_back();
+	}
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> n >> m;
+	dfs(1, 0);
+	
+	return 0;
+}
